1ff5f98554d2bd958ca366836d964f9c2b89573d
Lin-1997 <201921043987@mail.scut.edu.cn>
2020-04-10 11:46:46 +0800

    rename something

--- new/start.py
+++ new/start.py
@@ -12,18 +12,18 @@ worker_num_per_gpu = worker_num / gpu_num
 
 
 def create_worker (gpu_index, index, start_data_index, end_data_index):
-	# os.system("CUDA_VISIBLE_DEVICES=" + str(gpu_index) + " python w.py -i " + str(index) + " --start_data_index=" + str(start_data_idnex) + " --end_data_index=" + str(end_data_index))
-	os.system ("python w.py -i " + str (index) + " --start_data_index=" + str (
+	# os.system("CUDA_VISIBLE_DEVICES=" + str(gpu_index) + " python worker.py -i " + str(index) + " --start_data_index=" + str(start_data_idnex) + " --end_data_index=" + str(end_data_index))
+	os.system ("python worker.py -i " + str (index) + " --start_data_index=" + str (
 		start_data_index) + " --end_data_index=" + str (end_data_index))
 
 
-def create_parameter_server (worker_num, fraction):
-	# os.system ("CUDA_VISIBLE_DEVICES=0 python a.py -n " + str (worker_num) + " -f " + str (fraction))
-	os.system ("python a.py -n " + str (worker_num) + " -f " + str (fraction))
+def create_aggregator (worker_num, fraction):
+	# os.system ("CUDA_VISIBLE_DEVICES=0 python aggregator.py -n " + str (worker_num) + " -f " + str (fraction))
+	os.system ("python aggregator.py -n " + str (worker_num) + " -f " + str (fraction))
 
 
 for worker_index in range (worker_num):
 	threading.Thread (target=create_worker, args=(
 		int (worker_index / worker_num_per_gpu), worker_index, worker_index * each_worker_iid_data_num,
 		(worker_index + 1) * each_worker_iid_data_num)).start ()
-threading.Thread (target=create_parameter_server, args=(worker_num, fraction)).start ()
+threading.Thread (target=create_aggregator, args=(worker_num, fraction)).start ()
