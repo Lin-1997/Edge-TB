7260674cc60e3573427aef99ed62b5a36448a27d
ErianLiang <jungg1996@gmail.com>
2020-04-13 17:34:30 +0800

    Bug fixed: batch size 大于数据集大小

--- new/values/values_gossip.py
+++ new/values/values_gossip.py
@@ -2,7 +2,7 @@ values = {}
 
 values['start_port'] = 9990
 values['round'] = 40
-values['batch_size'] = 32
+values['batch_size'] = 1
 values['this_index'] = 0
 values['client_num'] = 3
 values['local_epoch_num'] = 1
